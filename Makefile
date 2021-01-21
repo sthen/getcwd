d735a7f37b25ff768568a858efbaa670687f55df
Stuart Henderson <stu@spacehopper.org>
2021-01-21 14:35:23 +0000

    add a script to getcwd from the current pid and call tmux splitw

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 PROG =	getcwd
 NOMAN =	getcwd
+SCRIPT = getcwd-splitw.sh
 
 WARNINGS = Yes
 
 BINDIR ?=	${HOME}/.local/bin
 BINOWN ?!=	id -un
 BINGRP ?!=	id -gn
+
+afterinstall:
+	${INSTALL} ${INSTALL_COPY} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE} \
+		${.CURDIR}/${SCRIPT} ${DESTDIR}${BINDIR}/getcwd-splitw
 
 .include <bsd.prog.mk>
---
+++ getcwd-splitw.sh
@@ -0,0 +1,18 @@
+#!/bin/ksh
+
+error() {
+	echo "$*" >&2 && exit 1
+}
+
+[ -z "${TMUX}" ] && error "tmux is not running"
+[ -o $# -lt 1 ] && error "usage: ${0##*/} <pid>"
+
+panel_cwd=`getcwd ${1}` || panel_cwd=`/bin/ps -o cwd= -p ${1}` || panel_cwd=.
+shift
+
+# if the current dir has since been removed, find the first existing parent
+while [ ! -d "${panel_cwd}" ] ; do
+	panel_cwd=`dirname "${panel_cwd}"`
+done
+
+exec /usr/bin/tmux split-window -c "${panel_cwd}" "$@"
